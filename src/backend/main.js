04fe04beb078fae145660e7f1be98266f066d0e4
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-05-14 02:50:39 +0300

    Add basic example with backend

--- src/backend/main.js
+++ src/backend/main.js
@@ -1,7 +1,22 @@
 'use strict';
 
-const main = () => {
-  console.log('Hello world!');
+const ws = require('ws');
+const config = require('./config.json');
+const database = require('knex')(config.database);
+
+const options = { ...config.network, clientTracking: true };
+
+const main = async () => {
+  const wss = new ws.WebSocketServer(options);
+  wss.on('connection', (socket) => {
+    let index = 0;
+    socket.on('message', (data) => {
+      const { id } = JSON.parse(data.toString());
+      const result = [{ message: 'Hello world!', own: false, index: index++ }];
+      const answer = { id, type: 'response', data: result };
+      socket.send(JSON.stringify(answer));
+    });
+  });
 };
 
 main()
--- src/frontend/main.js
+++ src/frontend/main.js
@@ -3,6 +3,80 @@
 const input = document.getElementById('messageInput');
 const chat = document.getElementById('chat');
 
+class AsyncQueue {
+  #futures = []
+  #values = []
+
+  put(value) {
+    if (this.#futures.length > 0) {
+      const future = this.#futures.shift();
+      return void future(value);
+    }
+    this.#values.push(value);
+  }
+
+  get() {
+    if (this.#values.length > 0) {
+      return Promise.resolve(this.#values.shift());
+    }
+    return new Promise((resolve) => {
+      this.#futures.push(resolve);
+    });
+  }
+
+  [Symbol.asyncIterator]() {
+    const next = async () => ({ value: await this.get(), done: false });
+    return { next };
+  }
+}
+
+class WebSocketTransport extends WebSocket {
+  #id = 0
+  #timeout = 0
+  #futures = new Map();
+  #values = new AsyncQueue()
+
+  constructor(url, timeout = 5000) {
+    super(url);
+    this.#timeout = timeout;
+    this.addEventListener('message', (event) => {
+      const payload = JSON.parse(event.data);
+      if (payload.type === 'response' && this.#futures.has(payload.id)) {
+        const { resolve, timer } = this.#futures.get(payload.id);
+        this.#futures.delete(payload.id);
+        resolve(payload.data);
+        return void clearTimeout(timer);
+      }
+      this.#values.put(payload.data);
+    });
+    return new Promise((resolve, reject) => {
+      const onOpen = () => {
+        this.removeEventListener('error', reject);
+        resolve(this);
+      };
+      this.addEventListener('open', onOpen, { once: true });
+      this.addEventListener('error', reject, { once: true });
+    });
+  }
+
+  send(data) {
+    const payload = { id: this.#id, data };
+    super.send(JSON.stringify(payload));
+    return new Promise((resolve, reject) => {
+      const onTimeout = () => {
+        reject(new Error('Response waiting time is up'));
+        this.#futures.delete(this.#id);
+      };
+      const timer = setTimeout(onTimeout, this.#timeout);
+      this.#futures.set(this.#id, { resolve, timer });
+    });
+  }
+
+  [Symbol.asyncIterator]() {
+    return this.#values[Symbol.asyncIterator]();
+  }
+}
+
 const putMessage = (message) => {
   const messageDiv = document.createElement('div');
   messageDiv.className = 'message my-message';
@@ -36,7 +110,14 @@ const generateRandomKey = (length = 16) => {
   return hex.join('');
 };
 
-(() => {
-  if (localStorage.getItem('id') !== null) return;
-  localStorage.setItem('id', generateRandomKey());
+(async () => {
+  if (localStorage.getItem('id') === null) {
+    localStorage.setItem('id', generateRandomKey());
+  }
+  
+  const ws = await new WebSocketTransport('ws://127.0.0.1:8080');
+  setInterval(async () => {
+    const answer = await ws.send({ service: 'messages', method: 'get' });
+    console.log({ answer });
+  }, 1000);
 })();
