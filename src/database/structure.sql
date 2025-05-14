c4477db47eaa8871d444709fb8123225af9eac38
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-05-14 18:52:20 +0300

    Rewrite database structure

--- src/database/structure.sql
+++ src/database/structure.sql
@@ -1,12 +1,12 @@
 create table "users" (
-  "id" char(16)
+  "id" char(32)
 );
 
 alter table "users" add constraint "pkUsers" primary key ("id");
 
 create table "messages" (
   "message"   text not null check (length("message") >= 1),
-  "userId"    char(16),
+  "userId"    char(32),
   "createdAt" timestamptz default current_timestamp
 );
 
