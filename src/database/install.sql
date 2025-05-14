ef3bb59c7a435febd20b012f1b69025167155950
Lytvynenko Serhii <serrheylitvinenko@gmail.com>
2025-05-14 21:13:13 +0300

    Added the ability to save data when restarting the container

--- src/database/install.sql
+++ src/database/install.sql
@@ -1,5 +1,3 @@
 
-drop database if exists global_chat;
-drop user if exists marcus;
-create user marcus with password 'marcus';
-create database global_chat owner marcus;
+create user if not exists marcus with password 'marcus';
+create database if not exists global_chat owner marcus;
