56530966a8d3f1ef8e1966ff77b643548371d614
Serhii Lytvynenko <serrheylitvinenko@gmail.com>
2025-05-14 18:52:01 +0300

    Update backend

---
+++ src/backend/api.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = (database) => ({
+  users: {
+    create: async ({ id }) => {
+      const user = await database('users').where({ id }).first();
+      if (user !== undefined) {
+        return { success: false, data: 'User already exists!' };
+      }
+      await database.insert({ id }).into('users');
+      return { success: true, data: { id } };
+    }
+  },
+
+  messages: {
+    get: async () => ({ success: true, data: await database('messages') }),
+  
+    create: async ({ userId, message }) => {
+      const user = await database('users').where({ id: userId }).first();
+      if (user === undefined) {
+        return { success: false, data: 'User does not exist!' };
+      }
+      const data = await database('messages').insert({ userId, message }, '*');
+      return { success: true, data };
+    }
+  }
+});
--- src/backend/main.js
+++ src/backend/main.js
@@ -3,20 +3,20 @@
 const ws = require('ws');
 const config = require('./config.json');
 const database = require('knex')(config.database);
+const api = require('./api.js')(database);
 
 const options = { ...config.network, clientTracking: true };
 
 const main = async () => {
   const wss = new ws.WebSocketServer(options);
   wss.on('connection', (socket) => {
-    let index = 0;
-    socket.on('message', (data) => {
-      const { id } = JSON.parse(data.toString());
-      const result = [{ message: 'Hello world!', own: false, index: index++ }];
-      const answer = { id, type: 'response', data: result };
-      socket.send(JSON.stringify(answer));
+    socket.on('message', async (rawData) => {
+      const { id, service, method, data = {} } = JSON.parse(rawData.toString());
+      const result = await api[service][method](data);
+      const response = { data: result, id, type: 'response' };
+      socket.send(JSON.stringify(response));
     });
   });
 };
 
-main()
+main();
